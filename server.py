e3559f4ef3fbe2d865cc4ce39b9ddf720a55516b
Jonathan Búcaro <44926768+B-caro@users.noreply.github.com>
2023-10-13 15:58:56 -0600

    Update server.py
    Signed-off-by: Jonathan Búcaro <44926768+B-caro@users.noreply.github.com>

--- server.py
+++ server.py
@@ -3,16 +3,30 @@ from flask import Flask, send_from_directory, render_template, redirect
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://www.banguat.gob.gt/tipo_cambio/')
-soup = BeautifulSoup(r.text, 'html.parser')
+app = Flask(__name__)
 
-fila = soup.find('tr', class_='detalle_banguat')
-if fila:
-    celda = fila.find('td', align='center')
-    if celda:
-        valor = celda.text
-        print("Valor en la celda:", valor)
+#port = int(os.environ.get("PORT", 5000))
+
+def obtener_precio(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    fila = soup.find('tr', class_='detalle_banguat')
+    if fila:
+        celda = fila.find('td', align='center')
+        if celda:
+            valor = celda.text
+            return valor
+        else:
+            return "No se encontró la celda con la alineación 'center'."
     else:
-        print("No se encontró la celda con la alineación 'center'.")
-else:
-    print("No se encontró la fila con la clase 'detalle_banguat'.")
+        return "No se encontró la fila con la clase 'detalle_banguat'."
+
+@app.route('/precio', methods=['GET'])
+def precio():
+    url = 'https://www.banguat.gob.gt/tipo_cambio/'
+    precio = obtener_precio(url)
+    return jsonify({"precio": precio})
+
+if __name__ == '__main__':
+    app.run(debug=True)
